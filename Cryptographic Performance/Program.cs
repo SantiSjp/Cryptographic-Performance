dd27cf257911dfde5428c69f530e6ac375f2f1be
santisjp <santisjp@live.com>
2020-10-04 17:30:53 -0300

    Add project files.

---
+++ Cryptographic Performance/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Cryptographic_Performance
+{
+    class Program
+    {
+
+
+        private static RSACryptoServiceProvider csp = new RSACryptoServiceProvider(2048);
+        private RSAParameters _privateKey;
+        private RSAParameters _publicKey;
+        static void Main()
+        {
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var rsa = new RSAEnc(2048);
+            string texto = "RSA e um algoritmo que deve o seu nome a tres professores do MIT: Ronald Rivest, Adi Shamir e Leonard Adleman";
+
+            Console.WriteLine(texto.Length);
+
+            
+            var publicKey = rsa.getPublicKey();
+            var privateKey = rsa.getPrivateKey();
+
+            var textoCifrado = rsa.Encrypt(texto, publicKey);
+
+            Console.WriteLine("Texto a ser cifrado: ");
+            Console.WriteLine(texto);
+            Console.WriteLine("------------------------------------------");
+
+            Console.WriteLine("Texto cifrado: ");
+            Console.WriteLine(textoCifrado);
+            Console.WriteLine("------------------------------------------");                   
+
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Tempo passado: {stopwatch.Elapsed}");
+        }
+
+    }
+}
---
+++ Cryptographic Performance/RSAEnc.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Cryptographic_Performance
+{
+    public class RSAEnc
+    {
+        public int EncBits { get; set; }
+
+        public RSAEnc(int encBits)
+        {
+            EncBits = encBits;
+        }
+
+        public string getPublicKey()
+        {
+            var cryptoServiceProvider = new RSACryptoServiceProvider(EncBits);
+            var publicKey = cryptoServiceProvider.ExportParameters(false);
+            string publicKeyString = GetKeyString(publicKey);
+
+            return publicKeyString;
+        }
+
+        public string getPrivateKey()
+        {
+            var cryptoServiceProvider = new RSACryptoServiceProvider(EncBits);
+            var privateKey = cryptoServiceProvider.ExportParameters(true);
+            string privateKeyString = GetKeyString(privateKey);
+
+            return privateKeyString;
+        }
+
+
+        public string Encrypt(string textToEncrypt, string keyString)
+        {
+            var bytesToEncrypt = Encoding.UTF8.GetBytes(textToEncrypt);
+
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                try
+                {
+                    rsa.FromXmlString(keyString.ToString());
+                    var encryptedData = rsa.Encrypt(bytesToEncrypt, true);
+                    var base64Encrypted = Convert.ToBase64String(encryptedData);
+                    return base64Encrypted;
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }
+
+
+        public string Decrypt(string textToDecrypt, string keyString)
+        {
+            var bytesToDescrypt = Encoding.UTF8.GetBytes(textToDecrypt);
+
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                try
+                {
+
+                    // server decrypting data with private key                    
+                    rsa.FromXmlString(keyString);
+
+                    var resultBytes = Convert.FromBase64String(textToDecrypt);
+                    var decryptedBytes = rsa.Decrypt(resultBytes, true);
+                    var decryptedData = Encoding.UTF8.GetString(decryptedBytes);
+                    return decryptedData.ToString();
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }
+
+
+        public string GetKeyString(RSAParameters Key)
+        {
+
+            var stringWriter = new System.IO.StringWriter();
+            var xmlSerializer = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+            xmlSerializer.Serialize(stringWriter, Key);
+            return stringWriter.ToString();
+        }
+
+    }
+}
